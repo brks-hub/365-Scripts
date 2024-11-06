23b4fd1391ad0c8d8eec801e597d61df25291bd5
brks-hub <daniel.brkovic@computingaustralia.group>
2024-11-06 15:21:38 +0800

    Update RemoveBloat.ps1

--- RemoveBloat.ps1
+++ RemoveBloat.ps1
@@ -2166,6 +2166,9 @@ if ($mcafeeinstalled -eq "true") {
                 }
     }
 
+# Create registry key to indicate script has successfully run
+New-Item -Path "HKLM:\SOFTWARE\CustomApp" -Force | Out-Null
+New-ItemProperty -Path "HKLM:\SOFTWARE\CustomApp" -Name "Installed" -Value "True" -PropertyType String -Force | Out-Null
 
 else {
     write-output "Intune detected, skipping removal of apps"
